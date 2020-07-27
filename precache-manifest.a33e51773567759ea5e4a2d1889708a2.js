9af17be3ad71dd17404f2b89e06f2d3d006aa973
apena415 <16394931+apena415@users.noreply.github.com>
2020-07-27 12:25:59 -0700

    Updates

---
+++ precache-manifest.a33e51773567759ea5e4a2d1889708a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec1b5e3f8223e9d968baa042be7e762",
+    "url": "/monsters-rolodex-with-hooks/index.html"
+  },
+  {
+    "revision": "31a260a34c11ff984aa4",
+    "url": "/monsters-rolodex-with-hooks/static/css/main.15a14929.chunk.css"
+  },
+  {
+    "revision": "e7af9c1231d56f75f096",
+    "url": "/monsters-rolodex-with-hooks/static/js/2.a5f68da1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-with-hooks/static/js/2.a5f68da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a260a34c11ff984aa4",
+    "url": "/monsters-rolodex-with-hooks/static/js/main.3748bdf1.chunk.js"
+  },
+  {
+    "revision": "0c603a394f5b3d09799a",
+    "url": "/monsters-rolodex-with-hooks/static/js/runtime-main.83ef733b.js"
+  }
+]);
